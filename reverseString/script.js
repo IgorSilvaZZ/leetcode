58140cc5b0d4615fa99a9bf631e6a4f35e528c95
IgorSilvaZZ <igor06silva12@gmail.com>
2026-03-24 13:26:52 -0300

    refactor: :recycle: adjusment logic in reverse string

--- reverseString/script.js
+++ reverseString/script.js
@@ -1,21 +1,30 @@
-let input = ["H", "a", "n", "n", "a", "h"];
+/* 
+  Input: s = ["h","e","l","l","o"]
+  Output: ["o","l","l","e","h"]
+*/
 
-const reverseString = (s) => {
-  let pointer1 = 0;
-  let pointer2 = s.length - 1;
+const input = ["h", "e", "l", "l", "o"];
 
-  while (pointer1 < pointer2) {
-    const firstLetter = s[pointer1];
-    const secundLetter = s[pointer2];
+/**
+ * @param {character[]} s
+ * @return {void} Do not return anything, modify s in-place instead.
+ */
+var reverseString = (s) => {
+	let left = 0;
+	let right = s.length - 1;
 
-    s[pointer1] = secundLetter;
-    s[pointer2] = firstLetter;
+	while (left < right) {
+		const aux = s[left];
+		const aux2 = s[right];
 
-    pointer1++;
-    pointer2--;
-  }
+		s[left] = aux2;
+		s[right] = aux;
+
+		left++;
+		right--;
+	}
 };
 
 reverseString(input);
 
-console.log({ input });
+console.log(input);
